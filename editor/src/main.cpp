badabca4b0f5ce7b48a585afd848a2cc8f38997f
Leonid Ovsyannikov <leo2004ovs@gmail.com>
2025-02-18 23:27:50 +0300

    Checkout the implemented logging system in the editor exe

--- editor/src/main.cpp
+++ editor/src/main.cpp
@@ -1,8 +1,20 @@
-#include <iostream>
-
 #include <uavpf/uavpf.h>
 
 int main()
 {
+	auto formatter = std::make_shared<uavpf::ConciseLogFormatter>();
+	auto emitter = std::make_shared<uavpf::ConsoleLogEmitter>();
+
+	uavpf::LoggerProvider::Get().
+		HostLogger(
+			uavpf::LogSource::Application,
+			std::move(formatter),
+			std::move(emitter));
+
+	UAVPF_LOG(Application, Trace, "Use this for USUAL things");
+	UAVPF_LOG(Application, Note, "Use this to CLARIFY things");
+	UAVPF_LOG(Application, Info, "Use this when something GOOD happends");
+	UAVPF_LOG(Application, Warning, "Use this when something SUSPICIOUS happens");
+	UAVPF_LOG(Application, Error, "Use this when something (VERY) BAD happens");
 }
 
