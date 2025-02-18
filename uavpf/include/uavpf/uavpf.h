b60d56be3c41c27972266dfc02d00fcd7a3f6f12
Leonid Ovsyannikov <leo2004ovs@gmail.com>
2025-02-18 22:55:15 +0300

    Update main library header file with logging headers

--- uavpf/include/uavpf/uavpf.h
+++ uavpf/include/uavpf/uavpf.h
@@ -4,5 +4,14 @@
 #include "uavpf/format_str.h"
 #include "uavpf/version.h"
 
+#include "uavpf/debug/formatted_log.h"
+#include "uavpf/debug/log_emitter.h"
+#include "uavpf/debug/log_formatter.h"
+#include "uavpf/debug/log_level.h"
+#include "uavpf/debug/log_record.h"
+#include "uavpf/debug/log_source.h"
+#include "uavpf/debug/logger.h"
+#include "uavpf/debug/logger_provider.h"
+
 #include "uavpf/id/base_id.h"
 
