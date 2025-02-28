f1da9029ae72925599d338a19c900b39cdfaf053
Leonid Ovsyannikov <leo2004ovs@gmail.com>
2025-03-01 01:21:59 +0300

    fix: not checking if call back registry is actually available

--- editor/src/event/event_dispatcher.cpp
+++ editor/src/event/event_dispatcher.cpp
@@ -4,6 +4,11 @@ namespace editor
 {
 	void EventDispatcher::Unsubscribe(const EventSubscription& subscription)
 	{
+		if (0 == mCallbackRegistriesByEventTypeID.count(subscription.EventTypeID))
+		{
+			return;
+		}
+
 		mCallbackRegistriesByEventTypeID
 			.at(subscription.EventTypeID)
 			->Unsubscribe(subscription.SubscriberID);
@@ -11,6 +16,11 @@ namespace editor
 
 	bool EventDispatcher::Notify(const Event& event)
 	{
+		if (0 == mCallbackRegistriesByEventTypeID.count(typeid(event)))
+		{
+			return false;
+		}
+
 		return mCallbackRegistriesByEventTypeID
 			.at(typeid(event))
 			->Notify(event);
