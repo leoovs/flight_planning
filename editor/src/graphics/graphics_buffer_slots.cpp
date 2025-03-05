04690772dab1e9dd622f11fb032d1cf11858f31f
Leonid Ovsyannikov <leo2004ovs@gmail.com>
2025-03-05 17:43:20 +0300

    Add container-wrapper to store buffers by slot index

---
+++ editor/src/graphics/graphics_buffer_slots.cpp
@@ -0,0 +1,46 @@
+#include "graphics/graphics_buffer_slots.h"
+
+#include <cassert>
+
+namespace editor
+{
+	GraphicsBuffer*& GraphicsBufferSlots::operator[](size_t slotIndex)
+	{
+		assert(slotIndex < mBuffersBySlot.size());
+		return mBuffersBySlot.at(slotIndex);
+	}
+
+	GraphicsBuffer* const& GraphicsBufferSlots::operator[](
+		size_t slotIndex) const
+	{
+		assert(slotIndex < mBuffersBySlot.size());
+		return mBuffersBySlot.at(slotIndex);
+	}
+
+	size_t GraphicsBufferSlots::ConvertIteratorToSlotIndex(
+		ConstIterator it) const
+	{
+		return std::distance(it, end());
+	}
+
+	GraphicsBufferSlots::Iterator GraphicsBufferSlots::begin()
+	{
+		return mBuffersBySlot.begin();
+	}
+
+	GraphicsBufferSlots::Iterator GraphicsBufferSlots::end()
+	{
+		return mBuffersBySlot.end();
+	}
+
+	GraphicsBufferSlots::ConstIterator GraphicsBufferSlots::begin() const
+	{
+		return mBuffersBySlot.begin();
+	}
+
+	GraphicsBufferSlots::ConstIterator GraphicsBufferSlots::end() const
+	{
+		return mBuffersBySlot.end();
+	}
+}
+
---
+++ editor/src/graphics/graphics_buffer_slots.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <array>
+
+namespace editor
+{
+	class GraphicsBuffer;
+
+	class GraphicsBufferSlots
+	{
+	public:
+		static constexpr size_t cMaxSlots = 8;
+
+		using Container = std::array<GraphicsBuffer*, cMaxSlots>;
+		using Iterator = Container::iterator;
+		using ConstIterator = Container::const_iterator;
+
+		GraphicsBuffer*& operator[](size_t slotIndex);
+		GraphicsBuffer* const& operator[](size_t slotIndex) const;
+
+		size_t ConvertIteratorToSlotIndex(ConstIterator it) const;
+
+		Iterator begin();
+		Iterator end();
+
+		ConstIterator begin() const;
+		ConstIterator end() const;
+
+	private:
+		Container mBuffersBySlot;
+	};
+}
+
