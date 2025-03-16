d519bf445c353b770e5406c33893b1921e392308
Leonid Ovsyannikov <leo2004ovs@gmail.com>
2025-03-16 14:02:55 +0300

    fix: native program not being replaced with 0 when released

--- editor/src/graphics_ogl/ogl_shader_compilation.cpp
+++ editor/src/graphics_ogl/ogl_shader_compilation.cpp
@@ -66,7 +66,7 @@ namespace editor
 
 	GLuint OglShaderCompilation::ReleaseNativeProgram()
 	{
-		return mNativeProgram;
+		return std::exchange(mNativeProgram, 0);
 	}
 
 	GLuint OglShaderCompilation::ReleaseNativeShader()
